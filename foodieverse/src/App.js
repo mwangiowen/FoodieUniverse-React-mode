866c69fdca811be6dfae96accd51e74c232188f4
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-11-10 12:09:46 +0300

    finished my App.js file

---
+++ foodieverse/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FoodApp from './componets/FoodApp';
+
+function App() {
+  return (
+    <div className="App">
+      <FoodApp />
+      
+    </div>
+  );
+}
+
+export default App;
